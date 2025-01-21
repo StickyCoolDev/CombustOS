b9a050db30e771b0e63da3eb65754c5fa3e347ba
StickyCoolDev <hujifa.info@gmail.com>
2025-01-21 10:00:38 +0600

    add functionality to arduino-nano.ino

--- Release/arduino-nano.ino
+++ Release/arduino-nano.ino
@@ -1,7 +1,33 @@
-void setup(){
-  
+// CombustOS 1.0 beta
+
+//library's 
+#include <LiquidCrystal.h> // initialize the library with the numbers of the interface pins – The numbers are the pin connected in sequence from RS to DB7
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);// Set it according to your screen placement
+
+
+
+// defines 
+
+//buttons
+#define buttonA 9
+#define ButtonB 10
+
+// add many buttons as you want defining them , add them to the array
+#define buttonArray [ButtonA, ButtonB]
+
+
+void setPORTB(byte value){
+  PORTB = value;
 }
 
-void loop(){
+void setup(){
+  for (buttons in buttonArray){
+    pinMode(buttons, INPUT_PULLDOWN)
+  };
+  
+  lcd.begin(16, 2); // Set your correct display height and width
+}
+
+void loop() {
   
 }
