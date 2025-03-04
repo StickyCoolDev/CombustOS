94a60a42d094916d9bf263fe9009fd038a09f153
StickyCoolDev <hujifa.info@gmail.com>
2025-03-04 15:31:04 +0600

    update arduino-nano.ino

--- Release/arduino-nano.ino
+++ Release/arduino-nano.ino
@@ -1,33 +1,74 @@
-// CombustOS 1.0 beta
-
-//library's 
-#include <LiquidCrystal.h> // initialize the library with the numbers of the interface pins – The numbers are the pin connected in sequence from RS to DB7
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);// Set it according to your screen placement
+// include the library code:
+#include <LiquidCrystal.h> // initialize the library with the numbers of the interface pins – The numbers
+//are the pin connected in sequence from RS to DB7
 
 
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-// defines 
+#define SelectedIcon 7
+#define NotSelectedIcon 6
+#define MoveButton 8
 
-//buttons
-#define buttonA 9
-#define ButtonB 10
+int SelectedPage = 1;
 
-// add many buttons as you want defining them , add them to the array
-#define buttonArray [ButtonA, ButtonB]
+byte Selected[] = { B00000, B00000, B00100, B01110, B11111, B01110, B00100, B00000 };
+byte NotSelected[] = { B00000, B00000, B00000, B00100, B01110, B00100, B00000, B00000 };
 
-
-void setPORTB(byte value){
-  PORTB = value;
-}
 
 void setup(){
-  for (buttons in buttonArray){
-    pinMode(buttons, INPUT_PULLDOWN)
-  };
+  lcd.begin(16, 2);
+  lcd.createChar(SelectedIcon, Selected);
+  lcd.setCursor(1, 0);
   
-  lcd.begin(16, 2); // Set your correct display height and width
+  
+  lcd.createChar(NotSelectedIcon, NotSelected);
+  
+  pinMode(MoveButton, INPUT);
+  //attachInterrupt((MoveButton), NextPage, FALLING);
+  NextPage();
+}
+
+void NextPage(){
+  lcd.clear();
+  if (SelectedPage >= 9)
+  {
+    SelectedPage = 1;
+  }else
+  {
+    SelectedPage++;
+  }
+  for (int i = 1; i <= 9; i++){
+    lcd.setCursor(i+3, 1);
+    if (i == SelectedPage)
+    {
+      lcd.write(SelectedIcon);
+      lcd.print(" ");
+      continue;
+    }
+    lcd.write(NotSelectedIcon);
+    lcd.print(" ");
+    
+  }
+  if (SelectedPage == 1){
+    lcd.setCursor(1, 0);
+    lcd.print("reset");
+  }
+  if (SelectedPage == 2){
+    lcd.setCursor(1, 0);
+    lcd.print("cool");
+  }
+  if (SelectedPage == 3){
+    lcd.setCursor(1, 0);
+    lcd.print("LCD screen");
+  }
 }
 
 void loop() {
   
+  if (digitalRead(MoveButton) == HIGH){
+    NextPage();
+    delay(200);
+  }
+  
+  //Set page
 }
