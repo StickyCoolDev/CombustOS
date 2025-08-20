f261342d725d17873f1c16e1ea9eadf2d38d8d2e
StickyCoolDev <hujifa.info@gmaill.com>
2025-08-20 23:43:45 +0600

    Add TUI engine class and SceneData
    this are only the outline of the full class without any functionality.

--- arduino-nano.ino
+++ arduino-nano.ino
@@ -1,74 +1,35 @@
-// include the library code:
-#include <LiquidCrystal.h> // initialize the library with the numbers of the interface pins – The numbers
-//are the pin connected in sequence from RS to DB7
+#include <LiquidCrystal.h>
 
-
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
-
-#define SelectedIcon 7
-#define NotSelectedIcon 6
-#define MoveButton 8
-
-int SelectedPage = 1;
-
-byte Selected[] = { B00000, B00000, B00100, B01110, B11111, B01110, B00100, B00000 };
-byte NotSelected[] = { B00000, B00000, B00000, B00100, B01110, B00100, B00000, B00000 };
-
-
-void setup(){
-  lcd.begin(16, 2);
-  lcd.createChar(SelectedIcon, Selected);
-  lcd.setCursor(1, 0);
-  
-  
-  lcd.createChar(NotSelectedIcon, NotSelected);
-  
-  pinMode(MoveButton, INPUT);
-  //attachInterrupt((MoveButton), NextPage, FALLING);
-  NextPage();
-}
-
-void NextPage(){
-  lcd.clear();
-  if (SelectedPage >= 9)
-  {
-    SelectedPage = 1;
-  }else
-  {
-    SelectedPage++;
-  }
-  for (int i = 1; i <= 9; i++){
-    lcd.setCursor(i+3, 1);
-    if (i == SelectedPage)
-    {
-      lcd.write(SelectedIcon);
-      lcd.print(" ");
-      continue;
-    }
-    lcd.write(NotSelectedIcon);
-    lcd.print(" ");
+class PixelByPixel { // the name of the text user interface is pixel by pixel
+public:
     
-  }
-  if (SelectedPage == 1){
-    lcd.setCursor(1, 0);
-    lcd.print("reset");
-  }
-  if (SelectedPage == 2){
-    lcd.setCursor(1, 0);
-    lcd.print("cool");
-  }
-  if (SelectedPage == 3){
-    lcd.setCursor(1, 0);
-    lcd.print("LCD screen");
-  }
-}
+    PixelByPixel(LiquidCrystal &lcd_ref) : m_Screen(&lcd_ref){};
+    void Init(unsigned short height, unsigned short width){
+        // set the passed in values to memeber variables
+        m_ScreenHeight = height;
+        m_ScreenWidth = width;
+    }; // like initrc from ncurses // it's also a separate function from the constructor as the initialisation of the liquid crystal glass is also not in its constructor
+    void Update();
+    void RenderScene(SceneData &ScreenToRender); // we will call the write , move, print on a scene class before rendering it by the TUI engine
+private:
 
-void loop() {
-  
-  if (digitalRead(MoveButton) == HIGH){
-    NextPage();
-    delay(200);
-  }
-  
-  //Set page
-}
+    LiquidCrystal *m_Screen; // using the screen as a reference
+
+    // variables for height and weight
+    unsigned short m_ScreenHeight; // unsigned shorts for memory
+    unsigned short m_ScreenWidth; // usage improvement
+};
+
+class SceneData {
+public:
+    SceneData();
+
+    virtual void OnRender(){
+
+    }
+private:
+    char m_Data[16][2]; // set a constant value for now 
+};
+
+void setup(){}
+void loop(){}
